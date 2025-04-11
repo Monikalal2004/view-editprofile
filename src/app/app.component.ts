87bdcbd5a271258a5a219091591fd49d63571f5d
unknown <Monikalal>
2025-04-11 11:33:25 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewprofileComponent } from './viewprofile/viewprofile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ViewprofileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/viewprofile/viewprofile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewprofile',
+  imports: [],
+  templateUrl: './viewprofile.component.html',
+  styleUrl: './viewprofile.component.css'
+})
+export class ViewprofileComponent {
+
+}
